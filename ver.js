d510655a5ecc30cf0c1d5db971e4aa5e53b7b810
integral-solutions <integral-solutions@users.noreply.github.com>
2026-03-05 15:07:53 -0300

    Delete ver.js

--- ver.js
+++
@@ -1,9 +0,0 @@
-<html>
-  <body>
-    <script src="https://js.puter.com/v2/"></script>
-    <script>
-      const authToken = await getAuthToken(); // performs browser based auth
-      const puter = init(authToken);
-    </script>
-  </body>
-</html>
